d61150c2a4d77dd6db6df17553f4429211708501
curiouslearner97 <curiouslearner12345@gmail.com>
2018-12-20 21:31:42 +0530

    new

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
 print("how are u?")
 print("hello world")
-<<<<<<< HEAD
 print("0th line")
 print("first line")
 print("second line")
 l1
-=======
-print("00th line")
-print("first 1st line")
-print("second line")
->>>>>>> first
